f1cc42617368afcac193a6d379161b3a57beff8e
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-06-18 14:08:51 +0000

    modification of the inside loop

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,9 @@
+import random
+
 def insertion_sort(array):
     '''
-    function to perform insertion sort algorithms
+    perform insertion sort algorithms
     return a sorted array of the n element of the array
-    
     '''
 
     checker = True
@@ -12,12 +13,18 @@ def insertion_sort(array):
 
         for i in range (1,len(array)):
             if array[i-1]>array[i]:
+                print( array[i-1], array[i])
                 checker = True
                 array[i-1], array[i] = array[i], array[i-1]
+            else:
+        
+                break
+
+    
     return array
 
+numbers = [6.441882568521997, 9.129469771028411, 8.085007999916666, 0.6830121262619548, 5.719945981926121, 3.3625403059347168, 0.0619248581640619, 1.2868810913334217, 4.704578031092196, 2.8700843094668347]
 
-numbers = [20,30, -10, 33, 10]
 print(insertion_sort(numbers))
             
 
