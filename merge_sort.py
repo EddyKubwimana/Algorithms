61109dd756197b8682e3ba0745b15ea46dba3fd0
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-06-24 20:43:10 +0000

    added the merge-sort function in process

--- merge_sort.py
+++ merge_sort.py
@@ -1,13 +1,54 @@
 # Divide and conquer algorithm
 def merge(left,right):
-    '''- Merge two arrays together'''
-    '''- Returns a a single array'''
+    '''- Merge two arrays together
+       - Returns a combined array
+    '''
     merged = []
     right_index, left_index = 0, 0
     while left_index<len(left) and right_index < len(right):
         
-        if left[left_index] >= right[right_index]:
+        if left[left_index] <=right[right_index]:
+            merged.append(left[left_index])
+            left_index+= 1
+        else:
+            merged.append(right[right_index])
+            right_index+=1
+
+    while left_index <len(left):
+         merged.append(left[left_index])
+         left_index+= 1
         
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index+=1
+
+    return merged
+
+def merge_sort(array):
+    '''
+    - Takes array of integer
+    - Return a sorted array
+    - Use nlogn runtime in the worst case scenario
+    '''
+
+    
+
+
+
+
+        
+        
+
+#.....................testing the function written above.....................#
+
+list1= [1,6,9,11]
+list2 = [3,4,5,10]
+
+print(merge(list1,list2))
+
+
+
+
 
 
     
