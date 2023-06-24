bf365984fe0d941a78d34e16f75a8a9568633796
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-06-24 20:32:21 +0000

    defining the merge sort function

---
+++ merge_sort.py
@@ -0,0 +1,18 @@
+# Divide and conquer algorithm
+def merge(left,right):
+    '''- Merge two arrays together'''
+    '''- Returns a a single array'''
+    merged = []
+    right_index, left_index = 0, 0
+    while left_index<len(left) and right_index < len(right):
+        
+        if left[left_index] >= right[right_index]:
+        
+
+
+    
+    
+    
+
+    
+    
