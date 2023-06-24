6371c060d507208108aee7bd3e7b08baf343cb5a
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-06-24 10:48:14 +0000

    change in naming the function

--- print.py
+++ print.py
@@ -1,11 +1,11 @@
-def printf(number):
+def list_n(number):
     '''Print numbers from 0 to n given number'''
 
     if number==0:
        print(number, end = ",")
        return number
     else:
-       printf(number-1)
+       list_n(number-1)
        print(number, end = ",")
     
-printf(10)
+list_n(10)
