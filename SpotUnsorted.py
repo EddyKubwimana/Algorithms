dee75fd4b423522890eedee601975cf2eb38dd2f
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-06-25 13:23:29 +0000

    added module for spot unsorted array

---
+++ SpotUnsorted.py
@@ -0,0 +1,11 @@
+# Writing algorithm that use 
+
+def spotsorted(array):
+    '''
+    - Takes an array
+    - Return True if the array is sorted
+    - Return False if the array is not sorted
+    - Use O (n log n)
+
+    '''
+    
