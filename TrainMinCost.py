1b9f9d1ea25a20f0cc2e8fa3ec35febac341fcd9
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-07-10 19:26:54 +0000

    new change

--- TrainMinCost.py
+++ TrainMinCost.py
@@ -1,6 +1,9 @@
 #Train min cost given a list of station and changing price for one station to another
 
 def minCost(arr,start,end):
+    ''' 
+    - Find the least cost of travelling from one point to another
+    '''
     start = start-1
     end = end-start-1
     arr = arr[start:]+arr[:start]
@@ -10,14 +13,23 @@ def minCost(arr,start,end):
 
     for i in range(end):
         right_cost += arr[i]
+
+
+    # left_cost:
+    left_cost = arr[0]
+    end = end
+    for i in range(len(arr)-1,end, -1 ):
+        left_cost += arr[i]
+        print(arr[i])
         
     
-    return arr, right_cost
+    return arr, right_cost, left_cost
 
 
     
 
 
 numbers = [1,6,9,85,0,9]
-print(minCost(numbers,2,4))
+print(numbers)
+print(minCost(numbers,1,2))
 
